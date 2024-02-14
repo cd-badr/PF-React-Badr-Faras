7a5f13dfbd4bf93ed12224a33036719cbe4604fc
cd-badr <badr@gmail.com>
2024-02-14 23:02:22 +0100

    add sec-2

--- src/pages/Home/components/firstSection.jsx
+++ src/pages/Home/components/firstSection.jsx
@@ -11,9 +11,9 @@ export const FirstSection = () => {
 
     return (
         <>
-            <div className="h-[70vh] w-full">
+            <div className="h-[60vh] w-full">
                 <Carousel>
-                    <div className="h-screen car1 flex flex-col justify-center items-center gap-5">
+                    <div className="h-[100vh] car1 flex flex-col justify-center items-center gap-5">
                         <p className='text-2xl text-white'>Women Collection 2018</p>
                         <h1 className='text-6xl text-white'>NEW ARRIVALS</h1>
                         <Link className='bg-white hover:bg-red-600 hover:text-white h-12 w-36 rounded-full flex justify-center items-center'>SHOP NOW</Link>
@@ -29,6 +29,41 @@ export const FirstSection = () => {
                         <Link className='bg-white hover:bg-red-600 hover:text-white h-12 w-36 rounded-full flex justify-center items-center'>SHOP NOW</Link>
                     </div>
                 </Carousel>
+            </div>
+        {/* sec-2 */}
+            <div className="flex flex-row justify-center p-5 gap-10">
+                <div className="left flex flex-col gap-10">
+                    <div className="l-img1 w-[350px] h-[450px] bg-cover bg-center flex justify-center items-end ">
+                    <Link className='bg-white hover:bg-red-600 hover:text-white h-12 w-48 rounded-sm flex justify-center items-center mb-6'>DRESSES</Link>
+                    </div>
+                    <div className="l-img2 w-[350px] h-[350px] bg-cover bg-center flex justify-center items-end">
+                    <Link className='bg-white hover:bg-red-600 hover:text-white h-12 w-48 rounded-sm flex justify-center items-center mb-6'>SUNGLASSES</Link>
+
+                    </div>
+                </div>
+
+                <div className="center flex flex-col gap-10">
+                    <div className="c-img1 w-[350px] h-[350px] bg-cover bg-center flex justify-center items-end">
+                    <Link className='bg-white hover:bg-red-600 hover:text-white h-12 w-48 rounded-sm flex justify-center items-center mb-6'>WATCHES</Link>
+
+                    </div>
+                    <div className="c-img2 w-[350px] h-[450px] bg-cover bg-center flex justify-center items-end">
+                    <Link className='bg-white hover:bg-red-600 hover:text-white h-12 w-48 rounded-sm flex justify-center items-center mb-6'>FOOTERWEAR</Link>
+
+                    </div>
+                </div>
+
+                <div className="right flex flex-col gap-10">
+                    <div className="r-img1 w-[350px] h-[450px] bg-cover bg-center flex justify-center items-end">
+                    <Link className='bg-white hover:bg-red-600 hover:text-white h-12 w-48 rounded-sm flex justify-center items-center mb-6'>BAGS</Link>
+
+                    </div>
+                    <div className="r-img2 w-[350px] h-[350px] bg-cover bg-center flex justify-center items-end">
+                    <Link className='bg-white hover:bg-red-600 hover:text-white h-12 w-48 rounded-sm flex justify-center items-center mb-6'>ACCESSORIES</Link>
+
+                    </div>
+                </div>
+
             </div>
         </>
     );
